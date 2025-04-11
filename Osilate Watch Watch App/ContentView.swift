b0ca08fffab801f0f48ff3e52be91fe709cd4cba
Zach Gottlieb <zsgott@gmail.com>
2025-04-11 11:09:48 -0700

    Add watchOS target.

---
+++ Osilate Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Osilate Watch Watch App
+//
+//  Created by Zach Gottlieb on 4/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Osilate Watch Watch App/Osilate_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Osilate_WatchApp.swift
+//  Osilate Watch Watch App
+//
+//  Created by Zach Gottlieb on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Osilate_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
