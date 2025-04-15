6d2f7ffa63068e9804aa20af4a04c44a30c213a3
Zach Gottlieb <zsgott@gmail.com>
2025-04-14 21:43:21 -0700

    Improve update frequency of zones view.

--- Osilate Watch Watch App/Session/ZonesView.swift
+++ Osilate Watch Watch App/Session/ZonesView.swift
@@ -11,78 +11,80 @@ struct ZonesView: View {
     @Environment(WorkoutManager.self) private var workoutManager
 
     var body: some View {
-        ZStack {
-            workoutManager.heartRate.zoneColor().opacity(0.2)
-            
-            GeometryReader { geometry in
-                VStack(alignment: .leading, spacing: 0) {
-                    ZStack(alignment: .leading) {
-                        RoundedRectangle(cornerRadius: 4)
-                            .foregroundStyle(.green.opacity(0.7))
-                            .frame(width: geometry.size.width * zonePercentage(.one), height: 34)
+        TimelineView(.periodic(from: Date(), by: 1.0)) { timeline in
+            ZStack {
+                workoutManager.heartRate.zoneColor().opacity(0.2)
+                
+                GeometryReader { geometry in
+                    VStack(alignment: .leading, spacing: 0) {
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 4)
+                                .foregroundStyle(.green.opacity(0.7))
+                                .frame(width: geometry.size.width * zonePercentage(.one), height: 28)
+                            
+                            Label {
+                                Text(formatTimeInterval(workoutManager.timeInZones[.one] ?? 0))
+                            } icon: {
+                                Image(systemName: "1.circle")
+                            }
+                        }
                         
-                        Label {
-                            Text(formatTimeInterval(workoutManager.timeInZones[.one] ?? 0))
-                        } icon: {
-                            Image(systemName: "1.circle")
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 4)
+                                .foregroundStyle(.yellow.opacity(0.7))
+                                .frame(width: geometry.size.width * zonePercentage(.two), height: 28)
+                            
+                            Label {
+                                Text(formatTimeInterval(workoutManager.timeInZones[.two] ?? 0))
+                            } icon: {
+                                Image(systemName: "2.circle")
+                            }
+                        }
+                        
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 4)
+                                .foregroundStyle(.orange.opacity(0.7))
+                                .frame(width: geometry.size.width * zonePercentage(.three), height: 28)
+                            
+                            Label {
+                                Text(formatTimeInterval(workoutManager.timeInZones[.three] ?? 0))
+                            } icon: {
+                                Image(systemName: "3.circle")
+                            }
+                        }
+                        
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 4)
+                                .foregroundStyle(.red.opacity(0.7))
+                                .frame(width: geometry.size.width * zonePercentage(.four), height: 28)
+                            
+                            Label {
+                                Text(formatTimeInterval(workoutManager.timeInZones[.four] ?? 0))
+                            } icon: {
+                                Image(systemName: "4.circle")
+                            }
+                        }
+                        
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 4)
+                                .foregroundStyle(.purple.opacity(0.7))
+                                .frame(width: geometry.size.width * zonePercentage(.five), height: 28)
+                            
+                            Label {
+                                Text(formatTimeInterval(workoutManager.timeInZones[.five] ?? 0))
+                            } icon: {
+                                Image(systemName: "5.circle")
+                            }
                         }
                     }
-                    
-                    ZStack(alignment: .leading) {
-                        RoundedRectangle(cornerRadius: 4)
-                            .foregroundStyle(.yellow.opacity(0.7))
-                            .frame(width: geometry.size.width * zonePercentage(.two), height: 34)
-                        
-                        Label {
-                            Text(formatTimeInterval(workoutManager.timeInZones[.two] ?? 0))
-                        } icon: {
-                            Image(systemName: "2.circle")
-                        }
-                    }
-                    
-                    ZStack(alignment: .leading) {
-                        RoundedRectangle(cornerRadius: 4)
-                            .foregroundStyle(.orange.opacity(0.7))
-                            .frame(width: geometry.size.width * zonePercentage(.three), height: 34)
-                        
-                        Label {
-                            Text(formatTimeInterval(workoutManager.timeInZones[.three] ?? 0))
-                        } icon: {
-                            Image(systemName: "3.circle")
-                        }
-                    }
-                    
-                    ZStack(alignment: .leading) {
-                        RoundedRectangle(cornerRadius: 4)
-                            .foregroundStyle(.red.opacity(0.7))
-                            .frame(width: geometry.size.width * zonePercentage(.four), height: 34)
-                        
-                        Label {
-                            Text(formatTimeInterval(workoutManager.timeInZones[.four] ?? 0))
-                        } icon: {
-                            Image(systemName: "4.circle")
-                        }
-                    }
-                    
-                    ZStack(alignment: .leading) {
-                        RoundedRectangle(cornerRadius: 4)
-                            .foregroundStyle(.purple.opacity(0.7))
-                            .frame(width: geometry.size.width * zonePercentage(.five), height: 34)
-                        
-                        Label {
-                            Text(formatTimeInterval(workoutManager.timeInZones[.five] ?? 0))
-                        } icon: {
-                            Image(systemName: "5.circle")
-                        }
-                    }
+                    .font(.title3.monospacedDigit().lowercaseSmallCaps())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .scenePadding()
+                    .padding(.top, 50)
                 }
-                .font(.title2.monospacedDigit().lowercaseSmallCaps())
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .scenePadding()
-                .padding(.top, 20)
             }
+            .ignoresSafeArea()
         }
-        .ignoresSafeArea()
     }
     
     // Helper function to calculate the percentage width for each zone
